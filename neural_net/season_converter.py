a8d879cc314db7c5f3ff6a239f6fe3dae324d414
Shereen Elaidi <shereen.elaidi@mail.mcgill.ca>
2018-06-07 14:51:51 -0400

    converted data

--- neural_net/season_converter.py
+++ neural_net/season_converter.py
@@ -1,29 +1,15 @@
 import csv
 import sys
-import random
 
-file = open(sys.argv[1], 'rt')
-reader = csv.reader(file)
-out = open('db_converted.csv', "wt")
-writer = csv.writer(out, delimiter=",")
+ifile = open(sys.argv[1], 'rt')
+reader = csv.reader(ifile)
+ofile = open('seasons.csv', "wt")
+writer = csv.writer(ofile, delimiter=',')
 
-rand = ['WINTER', 'SPRING', 'SUMMER', 'FALL']
 
 for row in reader:
-    if (row[1]=='Date'):
-        row[1] == 'Date'
-    elif (row[1]==''):
-        row[1] = random.choice(rand)
-    elif (int(row[1].split('/')[0]) >= 12 and int(row[1].split('/')[0]) <= 2):
-        row[1] == 'WINTER'
-    elif (int(row[1].split('/')[0]) >= 3 and int(row[1].split('/')[0]) <= 5):
-        row[1] == 'SPRING'
-    elif (int(row[1].split('/')[0]) >= 6 and int(row[1].split('/')[0]) <= 8):
-        row[1] == 'SUMMER'
-    elif(int(row[1].split('/')[0]) >= 9 and int(row[1].split('/')[0]) <= 11):
-        row[1] == 'FALL'
-
+    row[1] = row[1].split('/')[0]
     writer.writerow(row)
 
-file.close()
-out.close()
+ifile.close()
+ofile.close()
