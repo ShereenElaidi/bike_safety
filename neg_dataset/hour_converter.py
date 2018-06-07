a060cf4e6f7a56ba4c633c0fa007b19fb2c28be5
Yutong Yan <yutongyan@Yutongs-MacBook-Pro.local>
2018-06-07 13:52:19 -0400

    commit

--- neg_dataset/hour_converter.py
+++ neg_dataset/hour_converter.py
@@ -2,31 +2,29 @@ import csv
 import sys
 import random
 
+
 ifile = open(sys.argv[1], 'rt')
 reader = csv.reader(ifile)
-ofile  = open('data_base_converted.csv', "wt")
+ofile  = open(sys.argv[2], "wt")
 writer = csv.writer(ofile, delimiter=',')
 
 rand = ['RUSH HOUR', 'AM', 'PM']
 for row in reader:
-	#print(row[3].split(':')[0])
-	if row[2]=='':
-		row[2] = random.choice(rand)
-	if row[2].split(':')[0]>='12' and row[2].split(':')[0]<='15':
+	#print(row[2].split(':')[0])
+	if row[2].split(':')[0]>='12' and row[2].split(':')[0]<='15': 
 		row[2] = 'PM'
-	if row[2].split(':')[0]>='18' and row[2].split(':')[0]<='23':
+	elif row[2].split(':')[0]>='19' and row[2].split(':')[0]<='23': 
 		row[2] = 'PM'
-	if row[2].split(':')[0]>='7' and row[2].split(':')[0]<='8':
+	elif row[2].split(':')[0]>='7' and row[2].split(':')[0]<='9':
 		row[2] = 'RUSH HOUR'
-	if row[2].split(':')[0]>='16' and row[2].split(':')[0]<='17':
+	elif row[2].split(':')[0]>='16' and row[2].split(':')[0]<='18':
 		row[2] = 'RUSH HOUR'
-	if row[2].split(':')[0]>='9' and row[2].split(':')[0]<='11':
+	elif row[2].split(':')[0]>='10' and row[2].split(':')[0]<='11':
 		row[2] = 'AM'
-	if row[2].split(':')[0]>='0' and row[2].split(':')[0]<='6':
+	elif row[2].split(':')[0]>='0' and row[2].split(':')[0]<='6':
 		row[2] = 'AM'
-	if row[2].split(':')[0]=='9':
-		row[2] = 'AM'
-	
+	else:
+		row[2] = row[2]
 	writer.writerow(row)
 
 
