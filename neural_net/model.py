a90638a42a84b8e7bec10fad83d10b15de5ca854
Shereen Elaidi <shereen.elaidi@mail.mcgill.ca>
2018-06-07 11:31:56 -0400

    began working on the ML model

--- neural_net/model.py
+++ neural_net/model.py
@@ -1,88 +1,50 @@
-# import statements
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import numpy as np
 import pandas as pd
-import csv
-import time
-import requests
 
+# load the data
+df = pd.read_csv("converted_data.csv")
 
-addresses = []
-counts = []
-date = []
-year = []
-data_time = []
+# shuffling the dataframe
 
-# loading the data set
-data_set = pd.read_csv("data.csv")
+df = df.sample(frac=1)
 
-addresses = data_set['Geocoding address']
-counts = data_set['Duplicate count']
-date = data_set['Date']
-year = data_set['Year']
-data_time = data_set['Time']
+# Generating the training and validation sets
+np.random.seed(1234)
+train, validate, test = np.split(df.sample(frac=1, random_state=134),
+                                 [int(.6*len(df)), int(.8*len(df))])
 
+# We need to drop the count values for the training, validation, and test
+x_train = train.drop(['Count'], axis =1).values
+y_train = train['Count'].values
 
+x_val = train.drop(['Count'], axis=1).values
+y_val = train['Count'].values
 
-# a function to obtain the latitude and longitude of a given address.
-# Input: a string containing the address
-# Output: latitude, longitude of the address
-def get_coordinates(address):
-    url = 'https://maps.googleapis.com/maps/api/geocode/json?key=[API_KEY_HERE]'    #API key removed for privacy reasons
-    params = {'sensor': 'false', 'address': address}
-    r = requests.get(url, params=params)
-    results = r.json()['results']
-    location = results[0]['geometry']['location']
-    lat = str(location['lat'])
-    lng = str(location['lng'])
-    # print(lat + " " + lng)
-    return lat, lng
+x_test = test.drop(['Count'], axis=1).values
+y_test = test['Count'].values
 
-coordinates = []
-coordinates.append([])
-coordinates.append([])
+# creating the neural net
+torch.manual_seed(1234)
 
-# A function that will convert the entire dataset from address/intersection
-# to longitude and latitude.
-# Input: a list of addresses
-# Output: A list containing the latitude and longitude of each address.
-def address_to_coordinates(addresses):
-    m = len(list(csv.reader(open('data.csv'))))
-    data_base = open("data_base.txt", "w")
-    header = "Year\tDate\tTime\tCount\tLat\t\tLng\n"
-    data_base.write(header)
-    print(header)
-    for i in range (0,m):
-        t0 = time.time()            # Take note of the initial time
-        try:
-            curr_lat, curr_long = get_coordinates(addresses[i])
-            coordinates[0].append(curr_lat)
-            coordinates[1].append(curr_long)
-            curr_entry = str(year[i]) + "\t" + str(date[i]) + "\t" + str(data_time[i]) + "\t" + str(counts[i]) + "\t" + \
-                         str(curr_lat) + "\t" + str(curr_long) + "\n"
-            print(curr_entry)
-            data_base.write(curr_entry)
-            time.sleep(2)
-        except AttributeError:
-            print("Not an address.")
-        except IndexError:
-            data_base.write("NaN" + "\n")
-            print("NaN")
+class model(nn.Module):
+    def __init__(self):
+        super(model, self).__init__()
+        self.fc1 = nn.Linear(5, 20)
+        self.fc2 = nn.Linear(20, 40)
+        self.fc3 = nn.Linear(40, 20)
+        self.fc4 = nn.Linear(40, 20)
+        self.fc5 = nn.Linear(20, 2)         # Final output: danger (1) or no danger (2)
 
-        tf = time.time()            # Take note of the final time
+    def forward(self, x):
+        out = F.relu(self.fc1(x))
+        out = F.relu(self.fc2(out))
+        out = F.relu(self.fc3(out))
+        out = F.relu(self.fc4(out))
+        out = self.fc5(out)
 
-        if i%100==0:
-            print('Iteration: {}'.format(i))
-            print('Time {} s'.format(tf - t0))
-
-
-
-
-# lat = []
-# long = []
-#
-# for ....
-#     curr_lat, curr_long = find_coordinates(addresses[i])
-#     lat.append(curr_lat)
-#     long.append(curr_long)
-#     if i%100 == 0:
-#         save list ...
-#         print(i)
+        return out
