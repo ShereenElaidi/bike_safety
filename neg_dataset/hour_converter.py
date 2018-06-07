a9ea102710fac405e3f9d187d2735d9db61f9622
Shereen Elaidi <shereen.elaidi@mail.mcgill.ca>
2018-06-07 11:03:05 -0400

    converting data

--- neg_dataset/hour_converter.py
+++ neg_dataset/hour_converter.py
@@ -10,22 +10,22 @@ writer = csv.writer(ofile, delimiter=',')
 rand = ['RUSH HOUR', 'AM', 'PM']
 for row in reader:
 	#print(row[3].split(':')[0])
-	if row[3]=='': 
-		row[3] = random.choice(rand)
-	if row[3].split(':')[0]>='12' and row[3].split(':')[0]<='15': 
-		row[3] = 'PM'
-	if row[3].split(':')[0]>='18' and row[3].split(':')[0]<='23': 
-		row[3] = 'PM'
-	if row[3].split(':')[0]>='7' and row[3].split(':')[0]<='8':
-		row[3] = 'RUSH HOUR'
-	if row[3].split(':')[0]>='16' and row[3].split(':')[0]<='17':
-		row[3] = 'RUSH HOUR'
-	if row[3].split(':')[0]>='9' and row[3].split(':')[0]<='11':
-		row[3] = 'AM'
-	if row[3].split(':')[0]>='0' and row[3].split(':')[0]<='6':
-		row[3] = 'AM'
-	if row[3].split(':')[0]=='9':
-		row[3] = 'AM'
+	if row[2]=='':
+		row[2] = random.choice(rand)
+	if row[2].split(':')[0]>='12' and row[2].split(':')[0]<='15':
+		row[2] = 'PM'
+	if row[2].split(':')[0]>='18' and row[2].split(':')[0]<='23':
+		row[2] = 'PM'
+	if row[2].split(':')[0]>='7' and row[2].split(':')[0]<='8':
+		row[2] = 'RUSH HOUR'
+	if row[2].split(':')[0]>='16' and row[2].split(':')[0]<='17':
+		row[2] = 'RUSH HOUR'
+	if row[2].split(':')[0]>='9' and row[2].split(':')[0]<='11':
+		row[2] = 'AM'
+	if row[2].split(':')[0]>='0' and row[2].split(':')[0]<='6':
+		row[2] = 'AM'
+	if row[2].split(':')[0]=='9':
+		row[2] = 'AM'
 	
 	writer.writerow(row)
 
