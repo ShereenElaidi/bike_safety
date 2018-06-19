d9f5612ebd8e7e8929a3a32a908c210db2e962f2
Shereen Elaidi <shereen.elaidi@mail.mcgill.ca>
2018-06-19 11:03:49 -0400

    cleaned up code

--- neural_net/heat_map.py
+++ neural_net/heat_map.py
@@ -3,22 +3,12 @@ import gmplot
 from IPython.display import display
 import sys
 
+# A code to change the coordinates of the accident data-points to code for the heatmap.
+
 raw_data = pd.read_csv("data.csv")
-# display(raw_data.head(n=5))
-# display(raw_data.info())
 lat = raw_data['Lat']
 lng = raw_data['Lng']
-# print(lat)
-# print(lng)
-# gmap = gmplot.GoogleMapPlotter(45.5017, -73.5673, 10)
-# gmap.heatmap(lat, lng)
-# gmap.draw("my_heatmap.html")
-
-print(len(lat))
-print(len(lng))
 ofile = open("map_code", 'wt')
 for i in range (0,len(lat)):
     ofile.write("new google.maps.LatLng(%f, %f), \n" % (lat[i], lng[i]))
-
 ofile.close()
-
